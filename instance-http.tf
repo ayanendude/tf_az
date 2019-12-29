ff960c405b769992cfc2932e11e0023683b6cf67
ayanendu de <>
2019-12-29 01:47:00 -0500

    image

--- instance-http.tf
+++ instance-http.tf
@@ -23,7 +23,7 @@ resource "azurerm_virtual_machine" "http" {
   location              = azurerm_resource_group.generic.location
   resource_group_name   = azurerm_resource_group.generic.name
   network_interface_ids = [azurerm_network_interface.http.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_A1_v2"
 
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
