d31d7a9cdce5f8d78b8ad6a4d9ea524d93fed190
ayanendu de <>
2020-01-26 20:28:19 -0500

    no message

--- scripts/first-boot.sh
+++ scripts/first-boot.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "hello world"
+sudo apt-get update
+wget https://apt.puppetlabs.com/puppet6-release-bionic.deb
+sudo dpkg -i puppet6-release-bionic.deb
+sudo apt-get update
+sudo apt install -y puppet-agent
