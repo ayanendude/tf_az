f55181cf87cd3028f702b4d531ef17ac6d7e9ff2
ayanendu de <>
2020-03-19 21:54:33 -0400

    19 march

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
 output "resource_group_name" {
-    value = "$azurerm_resource_group.generic.name"
+    value = "${azurerm_resource_group.generic.name}"
 }
 output "resource_group_id" {
-    value = "$azurerm_resource_group.generic.id"
+    value = "${azurerm_resource_group.generic.id}"
 }
 output "lb_id" {
-    value = "$azurerm_lb.http.id"
+    value = "${azurerm_lb.http.id}"
 }
 output "subnet_id" {
-    value = "$azurerm_subnet.http.id"
+    value = "${azurerm_subnet.http.id}"
 }
